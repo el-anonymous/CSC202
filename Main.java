2b27a9ee8ad74b59e7e754d75a0cb06b36d8a215
Ebere-ElvisElvi <19873898-Ebere-ElvisElvi@users.noreply.replit.com>
2023-04-11 20:06:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    double PI = 3.142d;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+    System.out.println(circumference);
+  }
+}
